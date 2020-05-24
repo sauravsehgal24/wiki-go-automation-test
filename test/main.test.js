e5d50c130b2d092e9ee05c081d7c85370bd8eaff
sauravsehgal24 <sauravsehgal24@gmail.com>
2020-05-23 21:45:34 -0400

    changed code

--- test/main.test.js
+++ test/main.test.js
@@ -35,19 +35,6 @@ describe("Firefox",()=>{
     
   })
 
-
-
 })
 
 
-
-// (async function example() {
-//   let driver = await new Builder().forBrowser('firefox').build();
-//   try {
-//     await driver.get('http://www.google.com/ncr');
-//     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
